33e251544f764ac0f90dee8ac152cdbc72b22e5c
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-01-14 20:43:50 -0500

    had to fix mainmenu again.  Issue resolved.

--- header.php
+++ header.php
@@ -38,10 +38,10 @@
           <header id="top" role="banner" class="site-header homeContainer flex">
         <!-- <a href="#main" class="visuallyhidden focusable" id="skiptomain"></a> -->
         
-          <nav class="siteNavigation--Main mainMenu flex" role="navigation">
-              <!-- <ul class="mainMenu flex"> -->
+          <nav class="siteNavigation--Main flex" role="navigation">
+              <ul class="mainMenu flex">
                   <?php wp_nav_menu( array( "theme_location" => "primary", "container" => '', 'items_wrap'=> '%3$s' ) ); ?>
-              <!-- </ul>.menu -->
+              </ul> <!-- END OF MAINMENU -->
           </nav><!-- siteNavigation -->  
           
           </header> <!-- END OF CONTAINER -->
